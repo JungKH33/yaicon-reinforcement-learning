cabe66ca270d4ef3bb803713cd043dff67fe5da4
JungKH33 <melon345@yonsei.ac.kr>
2023-11-14 15:38:41 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,19 +6,34 @@ from Coach import Coach
 
 # othello
 from othello.OthelloGame import OthelloGame
-from othello.OthelloPlayers import *
 from othello.pytorch.NNet import NNetWrapper as OthelloNet
 
 # connect 4
 from connect4.Connect4Game import Connect4Game
-from connect4.Connect4Players import *
 from connect4.pytorch.NNet import NNetWrapper as Connect4Net
 
 # gobang
 from gobang.GobangGame import GobangGame
-from gobang.GobangPlayers import *
 from gobang.pytorch.NNet import NNetWrapper as GobangNet
 
+# quoridor
+#from quoridor.QuoridorGame import QuoridorGame
+#from quoridor.pytorch.NNet import NNetWrapper as QuoridorNet
+
+# gomoku
+from gomoku.pytorch.NNet import NNetWrapper as GomokuNet
+
+# yatch
+from yacht.YachtDiceGame import YachtDiceGame
+from yacht.pytorch.NNet import NNetWrapper as YachtNet
+
+# tafl
+from tafl.TaflGame import TaflGame
+from tafl.pytorch.NNet import NNetWrapper as YachtNet
+
+# dots and boxes
+from dotsandboxes.DotsAndBoxesGame import DotsAndBoxesGame
+from dotsandboxes.pytorch.NNet import NNetWrapper as DotboxNet
 
 from utils import *
 
@@ -28,7 +43,7 @@ coloredlogs.install(level='INFO')  # Change this to DEBUG to see more info.
 
 args = dotdict({
     'numIters': 1000,
-    'numEps': 50,              # Number of complete self-play games to simulate during a new iteration.
+    'numEps': 100,              # Number of complete self-play games to simulate during a new iteration.
     'tempThreshold': 15,        #
     'updateThreshold': 0.6,     # During arena playoff, new neural net will be accepted if threshold or more of games are won.
     'maxlenOfQueue': 200000,    # Number of game examples to train the neural networks.
@@ -38,7 +53,7 @@ args = dotdict({
 
     'checkpoint': './temp/',
     'load_model': False,
-    'load_folder_file': ('/dev/models/8x100x50','best.pth.tar'),
+    'load_folder_file': ('./pretrained_models/gomoku','best_policy_8_8_5.model'),
     'numItersForTrainExamplesHistory': 20,
 
 })
@@ -46,14 +61,23 @@ args = dotdict({
 
 def main():
     log.info('Loading %s...', GobangGame.__name__)
-    g = OthelloGame(8)
+    #g = OthelloGame(8)
     #g = Connect4Game()
-    g = GobangGame()
+    #g = GobangGame(8)
+    #g = QuoridorGame(5)
+    #g = YachtDiceGame()
+    #g = TaflGame()
+    g = DotsAndBoxesGame(3)
+
 
     log.info('Loading %s...', GobangNet.__name__)
-    nnet = OthelloNet(g)
+    #nnet = OthelloNet(g)
     #nnet = Connect4Net(g)
-    nnet = GobangNet(g)
+    #nnet = GobangNet(g)
+    #nnet = QuoridorGame(g)
+    #nnet = GomokuNet(g)
+    #nnet = YachtNet(g)
+    nnet = DotboxNet(g)
 
     if args.load_model:
         log.info('Loading checkpoint "%s/%s"...', args.load_folder_file[0], args.load_folder_file[1])
@@ -66,7 +90,7 @@ def main():
 
     if args.load_model:
         log.info("Loading 'trainExamples' from file...")
-        c.loadTrainExamples()
+        #c.loadTrainExamples()
 
     log.info('Starting the learning process 🎉')
     c.learn()
